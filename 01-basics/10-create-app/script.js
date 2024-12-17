1cfd797db9f9bdd17e4e721c74857bc3c0479e8c
Anastasia th9 <anastasiath9@Noutbuk-Anastasia.local>
2024-12-17 12:19:11 +0200

    answer: task 2

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,21 @@
 import { defineComponent, createApp } from 'vue'
+
+const Comp = defineComponent({
+    name: 'CurrentDate',
+    setup() {
+        function formatAsLocalDate(date) {
+            return new Date(date).toLocaleDateString(undefined, { dateStyle: 'long' });
+        }
+
+        const currentDate = new Date();
+
+        return {
+            formatAsLocalDate,
+            currentDate
+        };
+    },
+    template: `<div>Сегодня {{ formatAsLocalDate(currentDate) }}</div>`
+});
+
+
+createApp(Comp).mount("#app");
